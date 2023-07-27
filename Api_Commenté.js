fdad7842340b85d76a76c1cd8ff61b0718282f08
Célian Philibert <celian.philibert@gmail.com>
2023-07-27 19:55:53 +0200

    fin des commentaire api

--- Api_Commenté.js
+++ Api_Commenté.js
@@ -6,10 +6,10 @@ const dotenv = require('dotenv');
 
 /**
  * cette partie importe les modules requis en utilisant la fonction require de Node.js. Les modules importés sont :
- * -Express qui est un framework web utilisé pour créer des applications web et des API.
- * -Cors est un middleware permettant l'activation du partage de ressources entre origines (CORS), autorisant les ressources à être demandées depuis un autre domaine.
- * -Body-parser est un autre un middleware permettant de parser le corps de la requête dans différents formats et le rendant accessible via req.body.
- * -Dotenv est un module permettant de charger les variables d'environnement depuis un fichier .env dans l'objet process.env.
+ * -express qui est un framework web utilisé pour créer des applications web et des API.
+ * -cors est un middleware permettant l'activation du partage de ressources entre origines (CORS), autorisant les ressources à être demandées depuis un autre domaine.
+ * -body-parser est un autre un middleware permettant de parser le corps de la requête dans différents formats et le rendant accessible via req.body.
+ * -dotenv est un module permettant de charger les variables d'environnement depuis un fichier .env dans l'objet process.env.
  */
 
 //fichier de route
@@ -29,18 +29,38 @@ const salleRoutes = require('./src/routes/salleRoutes');
 const specialiteRoutes = require('./src/routes/specialiteRoutes');
 const typeSalleRoutes = require('./src/routes/typeSalleRoutes');
 
+/**
+ * Ces lignes importent différents fichiers de routes à partir d'emplacements différents dans le projet.
+ */
 
 //fichier variable d env
 dotenv.config({ path: './.local-env' });
+
+/**
+ * Cette ligne charge les variables d'environnement à partir du fichier .local-env 
+ * dans process.env. Cela permet à l'application d'accéder aux variables d'environnement définies dans ce fichier.
+ */
 
 //autres constantes
 const PORT = process.env.PORT || '3001';
 const app = express();
 
+/**
+ * Ici, le code définit la constante PORT avec soit la valeur de la variable d'environnement PORT, soit '3001' comme port par défaut. 
+ * Ensuite, il crée une instance de l'application Express et l'attribue à la variable app.
+ */
+
 //middleware
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
+
+/**
+ * ici les lignes configurent les middleware pour l'application Express.
+ * -express.json() va analyse les requêtes entrantes avec des corps JSON et rend les données analysées disponibles via req.body.
+ * -bodyParser.json() 
+ * -cors()va activer le partage de ressources entre origines (CORS) pour toutes les routes, autorisant les requêtes depuis n'importe quel domaine.
+ */
 
 //passage des routes
 //signRoutes(app);
@@ -61,13 +81,35 @@ app.use('/salle', salleRoutes);
 app.use('/specialite', specialiteRoutes);
 app.use('/typeSalle', typeSalleRoutes);
 
+/**
+ * Ces lignes spécifient les routes pour différents passages (par exemple, étudiant, intervenant, attacheProm, etc.) 
+ */
+
 app.use('/', (req, res, next) => {
     res.status(200).json({ serverPort: `${PORT}`, status: 'running' });
     return;
     //res.send(`<h1>Server du projet Signature sur le port ${PORT}</h1>`)
 })
 
+/**
+ * Cette ligne configure une route de dernier recours à la racine de l'application ('/'). 
+ * Lorsqu'une requête est faite vers cette racine, elle répond avec un objet JSON contenant le port du serveur et son état de fonctionnement. 
+ * La ligne mise en commentaire est une alternative pour répondre avec un message HTML.
+ */
+
 //ecoute du port
 app.listen(PORT, () =>
     console.log(`server started on port ${PORT}`)
 );
+
+/**
+ * cette ligne démarre le serveur Express et l'écoute sur le port spécifié par PORT. Lorsque le serveur démarre avec succès, 
+ * il affiche un message dans la console indiquant le port sur lequel il écoute.
+ */
+
+/**
+ * En résumé, ce code configure un serveur Express avec des middlewares pour gérer les requêtes, déclare des routes 
+ * pour différentes fonctionnalités, charge des variables d'environnement à partir d'un fichier .local-env, et met le serveur en écoute 
+ * sur un port spécifié (ou sur le port 3001 par défaut)
+ *  pour accepter les requêtes entrantes.
+ */
